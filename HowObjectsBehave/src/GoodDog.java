c7c92563dcca3ec3337add14f25f286e04934880
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2020-04-08 13:51:11 +0300

    Add files via upload

---
+++ HowObjectsBehave/src/GoodDog.java
@@ -0,0 +1,36 @@
+class GoodDog {
+    // Помечаем переменную экземпляра как private.
+    private int size;
+
+    // Помечаем геттеры и сеттеры как public
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int s) {
+        size = s;
+    }
+
+    void bark() {
+        if (size > 60) {
+            System.out.println("Гав Гав!");
+        } else if (size > 14) {
+            System.out.println("Вуф Вуф!");
+        } else {
+            System.out.println("Тяф Тяф!");
+        }
+    }
+}
+
+class GoodDogTestDrive {
+    public static void main(String[] args) {
+        GoodDog one = new GoodDog();
+        one.setSize(70);
+        GoodDog two = new GoodDog();
+        two.setSize(8);
+        System.out.println("Первая собака: " + one.getSize());
+        System.out.println("Вторая собака: " + two.getSize());
+        one.bark();
+        two.bark();
+    }
+}
