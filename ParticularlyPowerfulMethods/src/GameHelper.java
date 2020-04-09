b996bf12d7bb5296b50c0eecb2b3269bc3dc4a99
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2020-04-09 11:26:46 +0300

    Add files via upload

---
+++ ParticularlyPowerfulMethods/src/GameHelper.java
@@ -0,0 +1,37 @@
+import java.io.*;
+
+public class GameHelper {
+    public static void main(String[] args) {
+        int numOfGuesses = 0;
+        GameHelper helper = new GameHelper();
+
+        SimpleDotCom theDotCom = new SimpleDotCom();
+        int randomNum = (int) (Math.random() * 5);
+
+        int[] locations = {randomNum, randomNum+1, randomNum+2};
+        theDotCom.setLocationCells(locations);
+        boolean isAlive = true;
+
+        while (isAlive == true) {
+            String guess = helper.getUserInput("Введите число");
+            String result = theDotCom.checkYourself(guess);
+            numOfGuesses++;
+            if (result.equals("Потопил")) {
+                isAlive = false;
+                System.out.println("Вам потребовалось " + numOfGuesses + " попыток(и)");
+            }
+        }
+    }
+    public String getUserInput(String prompt) {
+        String inputLine = null;
+        System.out.print(prompt + " ");
+        try {
+            BufferedReader is = new BufferedReader(new InputStreamReader(System.in));
+            inputLine = is.readLine();
+            if (inputLine.length() == 0) return null;
+        } catch (IOException e) {
+            System.out.println("IOExeption: " + e);
+        }
+        return inputLine;
+    }
+}
