9cbe5a33a1197f560185f33e0e7d165afc1b22c9
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2020-04-10 10:28:23 +0300

    Add files via upload

---
+++ WonderfulLifeInObjectville/src/Doctor.java
@@ -0,0 +1,21 @@
+public class Doctor {
+    boolean worksAtHospital;
+    void treatPatient() {
+
+    }
+}
+
+class FamilyDoctor extends Doctor {
+    boolean makeHouseCalls;
+    void giveAdvice(){
+
+    }
+}
+class Surgeon extends Doctor {
+    void treatPatient() {
+
+    }
+    void makeIncision() {
+        
+    }
+}
