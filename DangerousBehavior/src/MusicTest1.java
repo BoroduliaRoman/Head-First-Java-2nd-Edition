debc7dff07c6262a3ea4f9c0471888752a074935
BoroduliaRoman <56869382+BoroduliaRoman@users.noreply.github.com>
2020-04-13 18:43:02 +0300

    Add files via upload

---
+++ DangerousBehavior/src/MusicTest1.java
@@ -0,0 +1,20 @@
+/*import javax.sound.midi.*;
+
+public class MusicTest1 {
+
+
+    public void play() {
+        try {
+            Sequencer sequencer = new MidiSystem.getSequencer();
+            System.out.println("Успешно получили синтезатор");
+        } catch (MidiUnavailableException ex) {
+            System.out.println("Неудача");
+        }
+    }
+
+    public static void main(String[] args) {
+        MusicTest1 mt = new MusicTest1();
+        mt.play();
+    }
+}
+*/
